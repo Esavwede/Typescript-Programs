95a7c7837046ac13060865b6c4539e945ec19124
ogagaoghene emmanuel <esavwede84@gmail.com>
2024-08-16 12:22:33 +0100

    added program3: program to print array types

---
+++ build/program3/arrays.js
@@ -0,0 +1,14 @@
+"use strict";
+function program3() {
+    arrays();
+}
+function arrays() {
+    let compromisedPasswords = [];
+    let specialCodes = [101, 303, 343, 233, 554, 111, 123];
+    compromisedPasswords.push('comp1', 'comp2', 'comp3');
+    console.log(" Compromized Passwords ");
+    console.log(compromisedPasswords);
+    console.log("Special Application Codes ");
+    console.log(specialCodes);
+}
+program3();
---
+++ src/program3/arrays.ts
@@ -0,0 +1,25 @@
+
+
+function program3()
+{
+    arrays() 
+}
+
+
+function arrays()
+{
+
+    let compromisedPasswords: string[] = [] 
+    let specialCodes: readonly number[] = [101, 303, 343, 233, 554, 111, 123 ]
+
+    compromisedPasswords.push('comp1','comp2','comp3') 
+
+    console.log(" Compromized Passwords ") 
+    console.log( compromisedPasswords) 
+
+    console.log("Special Application Codes ") 
+    console.log( specialCodes ) 
+}
+
+
+program3() 
