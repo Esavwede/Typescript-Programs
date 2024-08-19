e21ca34e1b9c6867362a5b187a7c162c3af6e473
ogagaoghene emmanuel <esavwede84@gmail.com>
2024-08-19 16:13:15 +0100

    added program9: a simple program that casts types in typescript

---
+++ build/program9/casting.js
@@ -0,0 +1,13 @@
+"use strict";
+function program9() {
+    var x = "2";
+    var y = 200;
+    console.log(" Y is of type ");
+    console.log(typeof y);
+    if (typeof x == "string") {
+        console.log(" X is a string ");
+        x = x;
+        console.log("Converted X to String ");
+    }
+}
+program9();
