b7f4a14c1d92e2683bfecf08528c18c559c43a4b
ogagaoghene emmanuel <esavwede84@gmail.com>
2024-08-21 15:00:57 +0100

    added program11: a simple program that utilizes generic types in typescript

---
+++ src/program11/generics.ts
@@ -0,0 +1,85 @@
+
+
+function program11()
+{
+    genericFunctions() 
+    genericTypes() 
+    genericClasses() 
+}
+
+
+
+
+function genericFunctions()
+{
+
+    // Receives a single Type 
+    function arrayPrinter<T>( array: T[] ): void 
+    {
+        for( let i = 0; i < array.length; i++ )
+        {
+            console.log(` Array ${ i }  value:  ${ array[i]}`)
+        }
+    }
+
+    // Example 
+    arrayPrinter(['a','b','c','d','e','f','g','h','i','j','k'])
+    arrayPrinter([100,200,300,400,500,600,700, 800, 900, 1000 ])
+    
+}
+
+function genericTypes()
+{
+    type WrappedValue<T> = { value: T } 
+
+    var wrappedValue1: WrappedValue<number> = { value: 10 }
+
+    console.log('wrapped value ')
+    console.dir( wrappedValue1 )  
+}
+
+
+function genericClasses()
+{
+
+    class Store<T> 
+    {
+        
+
+        constructor( private readonly id: string = 'somerandomid', private items: T[] )
+        {
+
+        }
+
+
+        getItems(): T[] 
+        {
+            return this.items 
+        }
+
+
+        addItem( item: T )
+        {
+            this.items.push( item ) 
+        }
+
+    }
+
+
+    interface Item 
+    {
+        name: string,
+        color: string,
+        weight?: number 
+    }
+
+    var myItems: Item[] = [ { name: 'basket', color: "blue "}, { name: 'mop', color:"red"  } ]
+    var myStore = new Store<Item>('fafdadfafd', myItems ) 
+
+    console.dir( myStore )
+
+    
+
+}
+
+program11() 
